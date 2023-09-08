7d647f6a9a159a156190e0dc4a69750046bdb540
Dr. Raed <drraed.jb@gmail.com>
2023-09-08 10:46:21 +0200

    data

--- Uebungen/urls.py
+++ Uebungen/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name="Uebungen"
+
 urlpatterns = [
-    path("", views.indexpage, name='indexpage')
+    path("", views.indexpage, name='indexpage'),
+    path("Artikel",views.ArtikelFrage, name='ArtikelFrage' ) ,  
 ]
